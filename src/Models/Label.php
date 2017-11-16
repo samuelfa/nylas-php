7ecae1ce9993520e156f44e9ac3d840cfdb0abee
兰林 <lanlin@lanlindeMacBook-Pro.local>
2017-11-16 10:08:25 +0800

    labels operate functions

--- src/Models/Label.php
+++ src/Models/Label.php
@@ -23,4 +23,79 @@ class Label extends Model
 
     // ------------------------------------------------------------------------------
 
+    /**
+     * @param string $labelName
+     * @param string $collectionName 'labels|folders'
+     * @return \Nylas\Models\Label
+     */
+    public function create(string $labelName, string $collectionName = 'labels')
+    {
+        $payload =
+        [
+            'display_name' => $labelName,
+        ];
+
+        $this->setCollection($collectionName);
+
+        $labelData  = $this->createResource($payload);
+        $this->data = $labelData->data;
+
+        return $this;
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * @param string $id
+     * @param string $newName
+     * @param string $collectionName 'labels|folders'
+     * @return \Nylas\Models\Label
+     */
+    public function update(string $id, string $newName, string $collectionName = 'labels')
+    {
+        $payload =
+        [
+            'display_name' => $newName,
+        ];
+
+        $this->setCollection($collectionName);
+
+        $labelData  = $this->updateResource($id, $payload);
+        $this->data = $labelData->data;
+
+        return $this;
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * @param string $id
+     * @param string $collectionName 'labels|folders'
+     * @return \Nylas\Models\Label
+     */
+    public function delete(string $id, string $collectionName = 'labels')
+    {
+        $this->setCollection($collectionName);
+
+        $labelData  = $this->deleteResource($id);
+        $this->data = $labelData->data;
+
+        return $this;
+    }
+
+    // ------------------------------------------------------------------------------
+
+    /**
+     * @param string $collectionName
+     */
+    private function setCollection(string $collectionName)
+    {
+        if ($collectionName && in_array($collectionName, ['labels', 'folders']))
+        {
+            $this->collectionName = $collectionName;
+        }
+    }
+
+    // ------------------------------------------------------------------------------
+
 }
